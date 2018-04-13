beb0ca3194b13aa1ee37b69febb351df380e13b1
Geng Zhang <zhanggeng.zg@antfin.com>
2018-04-13 11:06:57 +0800

    Prepare to release v5.3.0. (#9)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.3.0_20180112151111";
+    public static final String BUILD_VERSION = "5.3.0_20180413110000";
 }
