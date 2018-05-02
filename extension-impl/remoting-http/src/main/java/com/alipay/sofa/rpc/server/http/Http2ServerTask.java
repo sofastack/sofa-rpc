9a44fa6a5cae907c710dcc59b2c2ce1a6b85560b
leizhiyuan <leizhiyuan@gmail.com>
2018-05-02 18:59:21 +0800

    fix bug when SerializeType equals 0

--- extension-impl/remoting-http/src/main/java/com/alipay/sofa/rpc/server/http/Http2ServerTask.java
+++ extension-impl/remoting-http/src/main/java/com/alipay/sofa/rpc/server/http/Http2ServerTask.java
@@ -62,12 +62,15 @@ public class Http2ServerTask extends AbstractHttpServerTask {
 
     private void sendHttp2Response0(HttpResponseStatus status, boolean error, ByteBuf data) {
         Http2Headers headers = new DefaultHttp2Headers().status(status.codeAsText());
+
+        if (request.getSerializeType() > 0) {
+            String serialization = SerializerFactory.getAliasByCode(request.getSerializeType());
+            headers.set(RemotingConstants.HEAD_SERIALIZE_TYPE, serialization);
+        } else {
+            headers.set(CONTENT_TYPE, "text/plain; charset=" + RpcConstants.DEFAULT_CHARSET.displayName());
+        }
         if (error) {
             headers.set(RemotingConstants.HEAD_RESPONSE_ERROR, "true");
-            headers.set(CONTENT_TYPE, "text/plain; charset=" + RpcConstants.DEFAULT_CHARSET.displayName());
-        } else {
-            headers.set(RemotingConstants.HEAD_SERIALIZE_TYPE,
-                SerializerFactory.getAliasByCode(request.getSerializeType()));
         }
         if (data != null) {
             encoder.writeHeaders(ctx, streamId, headers, 0, false, ctx.newPromise());
