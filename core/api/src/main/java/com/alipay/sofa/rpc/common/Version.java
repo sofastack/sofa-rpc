d39d3b5e2a3c15d24c817efe241937bb84bd831d
leizhiyuan <leizhiyuan@gmail.com>
2018-05-02 18:59:21 +0800

    change rpc version to 5.5.0-SNAPSHOT

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.4.3";
+    public static final String VERSION       = "5.5.0";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50403;
+    public static final int    RPC_VERSION   = 50500;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.4.3_20180716134436";
+    public static final String BUILD_VERSION = "5.5.0_20180807203507";
 }
