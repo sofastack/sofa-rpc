783c6d54328f8e8a8f7befb7f8e4ed724ccc80be
leizhiyuan <leizhiyuan@gmail.com>
2018-05-02 18:59:21 +0800

    prepare to release 5.4.5

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.4.5_20180817103601";
+    public static final String BUILD_VERSION = "5.4.5_20180829115038";
 }
