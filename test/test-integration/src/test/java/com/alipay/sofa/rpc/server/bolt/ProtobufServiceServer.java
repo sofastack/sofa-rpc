c42e1a0a4ab7cba2a43c799d2c6e30fca7586113
Geng Zhang <zhanggeng.zg@antfin.com>
2018-05-11 11:53:05 +0800

    Add example and test case of bolt+protobuf. (#113)
    Fix #111

---
+++ test/test-integration/src/test/java/com/alipay/sofa/rpc/server/bolt/ProtobufServiceServer.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.rpc.server.bolt;
+
+import com.alipay.sofa.rpc.config.ProviderConfig;
+import com.alipay.sofa.rpc.config.ServerConfig;
+
+/**
+ * @author <a href="mailto:zhanggeng.zg@antfin.com">GengZhang</a>
+ */
+public class ProtobufServiceServer {
+
+    public static void main(String[] args) {
+        ServerConfig serverConfig = new ServerConfig()
+            .setProtocol("bolt") // 设置一个协议，默认bolt
+            .setPort(12200) // 设置一个端口，默认12200
+            .setDaemon(false); // 非守护线程
+
+        ProviderConfig<ProtobufService> providerConfig = new ProviderConfig<ProtobufService>()
+            .setInterfaceId(ProtobufService.class.getName()) // 指定接口
+            .setRef(new ProtobufServiceImpl()) // 指定实现
+            .setServer(serverConfig); // 指定服务端
+
+        providerConfig.export(); // 发布服务
+    }
+}
