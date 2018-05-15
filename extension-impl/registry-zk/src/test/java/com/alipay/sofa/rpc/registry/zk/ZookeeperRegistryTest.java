f8d38f3056651466c7c4496c1297da085af9a7a2
Nicholas Jiang <programgeek@163.com>
2018-05-15 19:24:10 +0800

    Support data of configs and overrides from zookeeper registry. (#100)

--- extension-impl/registry-zk/src/test/java/com/alipay/sofa/rpc/registry/zk/ZookeeperRegistryTest.java
+++ extension-impl/registry-zk/src/test/java/com/alipay/sofa/rpc/registry/zk/ZookeeperRegistryTest.java
@@ -18,11 +18,14 @@ package com.alipay.sofa.rpc.registry.zk;
 
 import com.alipay.sofa.rpc.client.ProviderGroup;
 import com.alipay.sofa.rpc.client.ProviderInfo;
+import com.alipay.sofa.rpc.common.RpcConstants;
+import com.alipay.sofa.rpc.common.utils.StringUtils;
 import com.alipay.sofa.rpc.config.ApplicationConfig;
 import com.alipay.sofa.rpc.config.ConsumerConfig;
 import com.alipay.sofa.rpc.config.ProviderConfig;
 import com.alipay.sofa.rpc.config.RegistryConfig;
 import com.alipay.sofa.rpc.config.ServerConfig;
+import com.alipay.sofa.rpc.listener.ConfigListener;
 import com.alipay.sofa.rpc.listener.ProviderInfoListener;
 import com.alipay.sofa.rpc.registry.RegistryFactory;
 import org.junit.AfterClass;
@@ -31,6 +34,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -38,8 +42,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
- *
- *
  * @author <a href="mailto:zhanggeng.zg@antfin.com">GengZhang</a>
  */
 public class ZookeeperRegistryTest {
@@ -67,8 +69,13 @@ public class ZookeeperRegistryTest {
         registry = null;
     }
 
+    /**
+     * 测试Zookeeper Provider Observer
+     *
+     * @throws Exception
+     */
     @Test
-    public void testAll() throws Exception {
+    public void testProviderObserver() throws Exception {
 
         int timeoutPerSub = 1000;
 
@@ -184,7 +191,122 @@ public class ZookeeperRegistryTest {
         List<ConsumerConfig> consumerConfigList = new ArrayList<ConsumerConfig>();
         consumerConfigList.add(consumer2);
         registry.batchUnSubscribe(consumerConfigList);
+    }
 
+    /**
+     * 测试Zookeeper Config Observer
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testConfigObserver() throws InterruptedException {
+        ServerConfig serverConfig = new ServerConfig()
+            .setProtocol("bolt")
+            .setHost("0.0.0.0")
+            .setPort(12200);
+
+        ProviderConfig<?> providerConfig = new ProviderConfig();
+        providerConfig.setInterfaceId("com.alipay.xxx.TestService")
+            .setUniqueId("unique123Id")
+            .setApplication(new ApplicationConfig().setAppName("test-server"))
+            .setProxy("javassist")
+            .setRegister(true)
+            .setRegistry(registryConfig)
+            .setSerialization("hessian2")
+            .setServer(serverConfig)
+            .setWeight(222)
+            .setTimeout(3000);
+
+        // 注册Provider Config
+        registry.register(providerConfig);
+
+        // 订阅Provider Config
+        CountDownLatch latch = new CountDownLatch(1);
+        MockConfigListener configListener = new MockConfigListener();
+        configListener.setCountDownLatch(latch);
+        registry.subscribeConfig(providerConfig, configListener);
+        configListener.attrUpdated(Collections.singletonMap("timeout", "2000"));
+        Map<String, String> configData = configListener.getData();
+        Assert.assertTrue(configData.size() == 1);
+        configListener.attrUpdated(Collections.singletonMap("uniqueId", "unique234Id"));
+        configData = configListener.getData();
+        Assert.assertTrue(configData.size() == 2);
+
+        ConsumerConfig<?> consumerConfig = new ConsumerConfig();
+        consumerConfig.setInterfaceId("com.alipay.xxx.TestService")
+            .setUniqueId("unique123Id")
+            .setApplication(new ApplicationConfig().setAppName("test-server"))
+            .setProxy("javassist")
+            .setSubscribe(true)
+            .setSerialization("java")
+            .setInvokeType("sync")
+            .setTimeout(4444);
+
+        // 订阅Consumer Config
+        latch = new CountDownLatch(1);
+        configListener = new MockConfigListener();
+        configListener.setCountDownLatch(latch);
+        registry.subscribeConfig(consumerConfig, configListener);
+        configListener.attrUpdated(Collections.singletonMap(RpcConstants.CONFIG_KEY_TIMEOUT, "3333"));
+        configData = configListener.getData();
+        Assert.assertTrue(configData.size() == 1);
+        configListener.attrUpdated(Collections.singletonMap("uniqueId", "unique234Id"));
+        configData = configListener.getData();
+        Assert.assertTrue(configData.size() == 2);
+
+        latch.await(2000, TimeUnit.MILLISECONDS);
+        Assert.assertTrue(configData.size() == 2);
+    }
+
+    /**
+     * 测试Zookeeper Override Observer
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testOverrideObserver() throws InterruptedException {
+        ConsumerConfig<?> consumerConfig = new ConsumerConfig();
+        consumerConfig.setInterfaceId("com.alipay.xxx.TestService")
+            .setUniqueId("unique123Id")
+            .setApplication(new ApplicationConfig().setAppName("test-server"))
+            .setProxy("javassist")
+            .setSubscribe(true)
+            .setSerialization("java")
+            .setInvokeType("sync")
+            .setTimeout(4444);
+
+        // 订阅Consumer Config
+        CountDownLatch latch = new CountDownLatch(1);
+        MockConfigListener configListener = new MockConfigListener();
+        configListener.setCountDownLatch(latch);
+        registry.subscribeOverride(consumerConfig, configListener);
+        Map<String, String> attributes = new ConcurrentHashMap<String, String>();
+        attributes.put(RpcConstants.CONFIG_KEY_TIMEOUT, "3333");
+        attributes.put(RpcConstants.CONFIG_KEY_APP_NAME, "test-server");
+        attributes.put(RpcConstants.CONFIG_KEY_SERIALIZATION, "java");
+        configListener.attrUpdated(attributes);
+        Map<String, String> configData = configListener.getData();
+        Assert.assertTrue(configData.size() == 3);
+
+        consumerConfig.setInterfaceId("com.alipay.xxx.TestService")
+            .setUniqueId("unique123Id")
+            .setApplication(new ApplicationConfig().setAppName("test-server1"))
+            .setProxy("javassist")
+            .setSubscribe(true)
+            .setSerialization("java")
+            .setInvokeType("sync")
+            .setTimeout(5555);
+        configListener = new MockConfigListener();
+        configListener.setCountDownLatch(latch);
+        registry.subscribeOverride(consumerConfig, configListener);
+        attributes.put(RpcConstants.CONFIG_KEY_TIMEOUT, "4444");
+        attributes.put(RpcConstants.CONFIG_KEY_APP_NAME, "test-server2");
+        configListener.attrUpdated(attributes);
+        configData = configListener.getData();
+        Assert.assertTrue(configData.size() == 3);
+
+        latch.await(2000, TimeUnit.MILLISECONDS);
+        Assert.assertTrue(configData.size() == 3);
     }
 
     private static class MockProviderInfoListener implements ProviderInfoListener {
@@ -235,6 +357,36 @@ public class ZookeeperRegistryTest {
 
         public Map<String, ProviderInfo> getData() {
             return ps;
+        }
+    }
+
+    private static class MockConfigListener implements ConfigListener {
+
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<String, String>();
+
+        private CountDownLatch            countDownLatch;
+
+        public void setCountDownLatch(CountDownLatch countDownLatch) {
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void configChanged(Map newValue) {
+        }
+
+        @Override
+        public void attrUpdated(Map newValue) {
+            for (Object property : newValue.keySet()) {
+                concurrentHashMap.put(StringUtils.toString(property), StringUtils.toString(newValue.get(property)));
+                if (countDownLatch != null) {
+                    countDownLatch.countDown();
+                    countDownLatch = null;
+                }
+            }
+        }
+
+        public Map<String, String> getData() {
+            return concurrentHashMap;
         }
     }
 }
