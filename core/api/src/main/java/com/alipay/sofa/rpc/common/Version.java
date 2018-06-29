27ef650c35901b5ba733fd29284d1d3619e96cb4
Geng Zhang <zhanggeng.zg@antfin.com>
2018-06-29 18:05:14 +0800

    Upgrade version to 5.4.2-SNAPSHOT. (#208)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.4.1";
+    public static final String VERSION       = "5.4.2";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50401;
+    public static final int    RPC_VERSION   = 50402;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.4.1_20180627153902";
+    public static final String BUILD_VERSION = "5.4.2_20180629105056";
 }
