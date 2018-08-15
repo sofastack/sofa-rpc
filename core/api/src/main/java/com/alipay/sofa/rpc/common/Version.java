1f582d5e1702936eacf719be7d6a914b8845c668
QilongZhang <qilong.zql@alibaba-inc.com>
2018-08-15 15:26:30 +0800

    upgrade version 5.4.4

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.4.4_20180720111452";
+    public static final String BUILD_VERSION = "5.4.4_20180815152457";
 }
