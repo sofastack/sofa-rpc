4eeaf0792505a12abd10c6a6c59afd4a1740e14d
moriadry <preciousdp11@gmail.com>
2018-09-10 08:52:02 +0800

    commit

--- extension-impl/bootstrap-dubbo/src/main/java/com/alipay/sofa/rpc/bootstrap/dubbo/DubboConsumerBootstrap.java
+++ extension-impl/bootstrap-dubbo/src/main/java/com/alipay/sofa/rpc/bootstrap/dubbo/DubboConsumerBootstrap.java
@@ -140,7 +140,7 @@ public class DubboConsumerBootstrap<T> extends ConsumerBootstrap<T> {
         referenceConfig.setId(consumerConfig.getId());
         referenceConfig.setInterface(consumerConfig.getInterfaceId());
         referenceConfig.setGroup(consumerConfig.getUniqueId());
-        referenceConfig.setVersion(consumerConfig.getUniqueId());
+        referenceConfig.setVersion(consumerConfig.getGroup());
         referenceConfig.setActives(consumerConfig.getConcurrents());
         referenceConfig.setCluster(consumerConfig.getCluster());
         referenceConfig.setConnections(consumerConfig.getConnectionNum());
