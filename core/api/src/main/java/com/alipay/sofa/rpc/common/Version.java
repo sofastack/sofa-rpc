31ee7a1af37c4acbbc35923af93841ae0cad81d5
leizhiyuan <leizhiyuan@gmail.com>
2018-11-09 11:55:40 +0800

    update rpc 5.4.8-SNAPSHOT

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.4.7";
+    public static final String VERSION       = "5.4.8";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50407;
+    public static final int    RPC_VERSION   = 50408;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.4.7_20181025093119";
+    public static final String BUILD_VERSION = "5.4.8_20181109115509";
 }
