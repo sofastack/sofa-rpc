74b501dc231d9fe0105d4e7177aaf6db925d6aac
zhaojigang <1197596604@qq.com>
2018-11-22 17:04:50 +0800

    Fix /issues/367 and remove unnecessary code. (#368)
    * fix /issues/367

--- core/api/src/main/java/com/alipay/sofa/rpc/common/struct/Cache.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/struct/Cache.java
@@ -29,7 +29,7 @@ public interface Cache {
      * @param args        方法参数
      * @return 关键字，可以返回null
      */
-    public Object buildKey(String interfaceId, String methodName, Object[] args);
+    Object buildKey(String interfaceId, String methodName, Object[] args);
 
     /**
      * 放入缓存
@@ -37,7 +37,7 @@ public interface Cache {
      * @param key    方法参数得到的关键字
      * @param result 缓存的调用结果
      */
-    public void put(Object key, Object result);
+    void put(Object key, Object result);
 
     /**
      * 拿出缓存
@@ -45,5 +45,5 @@ public interface Cache {
      * @param key 方法参数得到的关键字
      * @return 缓存的调用结果
      */
-    public Object get(Object key);
+    Object get(Object key);
 }
