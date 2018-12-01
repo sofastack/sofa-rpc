2b241a05ea51bda797ddde34d386fd9ade53ab45
LiangEn.liwei <lw111072@antfin.com>
2018-12-01 23:06:30 +0800

    fix test

--- test/test-integration/src/test/java/com/alipay/sofa/rpc/server/rest/RestTracerTest.java
+++ test/test-integration/src/test/java/com/alipay/sofa/rpc/server/rest/RestTracerTest.java
@@ -78,26 +78,26 @@ public class RestTracerTest extends ActivelyDestroyTest {
         memoryReporter = (MemoryReporterImpl) clientReporter;
 
         ServerConfig restServer = new ServerConfig()
-                .setPort(8583)
-                .setProtocol(RpcConstants.PROTOCOL_TYPE_REST);
+            .setPort(8583)
+            .setProtocol(RpcConstants.PROTOCOL_TYPE_REST);
 
         List<ServerConfig> servers = new ArrayList<ServerConfig>(2);
         servers.add(restServer);
 
         ProviderConfig<RestService> providerConfig = new ProviderConfig<RestService>()
-                .setInterfaceId(RestService.class.getName())
-                .setRef(new RestServiceImpl())
-                .setRegister(false)
-                .setServer(servers);
+            .setInterfaceId(RestService.class.getName())
+            .setRef(new RestServiceImpl())
+            .setRegister(false)
+            .setServer(servers);
         providerConfig.export();
 
         //rest服务
         ConsumerConfig<RestService> consumerConfigRest = new ConsumerConfig<RestService>()
-                .setInterfaceId(RestService.class.getName())
-                .setProtocol(RpcConstants.PROTOCOL_TYPE_REST)
-                .setDirectUrl("rest://127.0.0.1:8583")
-                .setTimeout(1000)
-                .setApplication(new ApplicationConfig().setAppName("TestClientRest"));
+            .setInterfaceId(RestService.class.getName())
+            .setProtocol(RpcConstants.PROTOCOL_TYPE_REST)
+            .setDirectUrl("rest://127.0.0.1:8583")
+            .setTimeout(1000)
+            .setApplication(new ApplicationConfig().setAppName("TestClientRest"));
         final RestService restServiceRest = consumerConfigRest.refer();
 
         restServiceRest.get("test");
@@ -153,7 +153,7 @@ public class RestTracerTest extends ActivelyDestroyTest {
         //validate one rpc server and rpc client field
 
         boolean result = TracerChecker.validateTracerDigest(clientDigest.get(0), "client",
-                RpcConstants.PROTOCOL_TYPE_REST);
+            RpcConstants.PROTOCOL_TYPE_REST);
 
         Assert.assertTrue(result);
         result = TracerChecker.validateTracerDigest(serverDigest.get(0), "server", RpcConstants.PROTOCOL_TYPE_REST);
