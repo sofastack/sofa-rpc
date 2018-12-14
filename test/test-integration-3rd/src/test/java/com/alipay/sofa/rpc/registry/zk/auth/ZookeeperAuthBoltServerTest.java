6380429277a013f0b4288507ef5b2bdd056b6372
蹇阳 <jianyang@asiainfo.com>
2018-12-14 15:16:20 +0800

    override zk auth test case

--- test/test-integration-3rd/src/test/java/com/alipay/sofa/rpc/registry/zk/auth/ZookeeperAuthBoltServerTest.java
+++ test/test-integration-3rd/src/test/java/com/alipay/sofa/rpc/registry/zk/auth/ZookeeperAuthBoltServerTest.java
@@ -20,9 +20,12 @@ import com.alipay.sofa.rpc.config.ConsumerConfig;
 import com.alipay.sofa.rpc.config.ProviderConfig;
 import com.alipay.sofa.rpc.config.RegistryConfig;
 import com.alipay.sofa.rpc.config.ServerConfig;
+import com.alipay.sofa.rpc.registry.base.BaseZkTest;
 import com.alipay.sofa.rpc.test.EchoService;
 import com.alipay.sofa.rpc.test.EchoServiceImpl;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -31,31 +34,44 @@ import java.util.Map;
 /**
  * @author <a href="mailto:jjzxjgy@126.com">jianyang</a>
  */
-public class ZookeeperAuthBoltServerTest {
+public class ZookeeperAuthBoltServerTest extends BaseZkTest {
+
+    private static ServerConfig   serverConfig;
+    private static RegistryConfig registryConfig;
+
+    private static Map<String, String> parameters = new HashMap<String, String>();
+
+    @BeforeClass
+    public static void setUp() {
+        parameters.put("scheme", "digest");
+        //如果存在多个认证信息，则在参数形式为为user1:passwd1,user2:passwd2
+        parameters.put("addAuth", "sofazk:rpc1");
+
+        registryConfig = new RegistryConfig()
+                .setProtocol("zookeeper")
+                .setAddress("127.0.0.1:2181/authtest")
+                .setParameters(parameters);
+
+        serverConfig = new ServerConfig()
+                .setProtocol("bolt") // 设置一个协议，默认bolt
+                .setPort(12200) // 设置一个端口，默认12200
+                .setDaemon(false); // 非守护线程
+    }
+
+
+    @AfterClass
+    public static void destroy() {
+        serverConfig.destroy();
+    }
 
     @Test
     public void testAll() {
 
-        Map<String, String> parameters = new HashMap<String, String>();
-        parameters.put("scheme", "digest");
-        //如果存在多个认证信息，则在参数形式为为user1:paasswd1,user2:passwd2
-        parameters.put("addAuth", "sofazk:rpc");
-
-        RegistryConfig registryConfig = new RegistryConfig()
-            .setProtocol("zookeeper")
-            .setAddress("127.0.0.1:2181/authtest")
-            .setParameters(parameters);
-
-        ServerConfig serverConfig = new ServerConfig()
-            .setProtocol("bolt") // 设置一个协议，默认bolt
-            .setPort(12200) // 设置一个端口，默认12200
-            .setDaemon(false); // 非守护线程
-
         ProviderConfig<EchoService> providerConfig = new ProviderConfig<EchoService>()
-            .setRegistry(registryConfig)
-            .setInterfaceId(EchoService.class.getName()) // 指定接口
-            .setRef(new EchoServiceImpl()) // 指定实现
-            .setServer(serverConfig); // 指定服务端
+                .setRegistry(registryConfig)
+                .setInterfaceId(EchoService.class.getName()) // 指定接口
+                .setRef(new EchoServiceImpl()) // 指定实现
+                .setServer(serverConfig); // 指定服务端
         providerConfig.export(); // 发布服务
 
         ConsumerConfig<EchoService> consumerConfig = new ConsumerConfig<EchoService>()
@@ -71,4 +87,6 @@ public class ZookeeperAuthBoltServerTest {
         Assert.assertEquals("auth test", result);
 
     }
+
+
 }
