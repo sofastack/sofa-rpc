69cb91f876f97e3885eae1a8e4d131c5d24431ba
leizhiyuan <leizhiyuan@gmail.com>
2019-04-11 17:04:07 +0800

    update rpc to 553 snapshot

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.5.2";
+    public static final String VERSION       = "5.5.3";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50502;
+    public static final int    RPC_VERSION   = 50503;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.5.2_20190404093733";
+    public static final String BUILD_VERSION = "5.5.3_20190411170240";
 }
