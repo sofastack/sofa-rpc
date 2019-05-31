d0a8e7405f6cd5ef1f5f7779824dfa1037b827be
zhaojigang <zhaojigang@corp.netease.com>
2019-05-31 10:43:59 +0800

    add sofaregsitry example

---
+++ example/src/test/java/com/alipay/sofa/rpc/sofaregistry/SofaRegistryClient.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.rpc.sofaregistry;
+
+import com.alipay.sofa.rpc.config.ConsumerConfig;
+import com.alipay.sofa.rpc.config.RegistryConfig;
+import com.alipay.sofa.rpc.log.Logger;
+import com.alipay.sofa.rpc.log.LoggerFactory;
+
+/**
+ * Quick Start client
+ */
+public class SofaRegistryClient {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SofaRegistryClient.class);
+
+    public static void main(String[] args) {
+        // 1. 注册中心配置
+        RegistryConfig registryConfig = new RegistryConfig()
+                .setProtocol("sofa") // 设置协议
+                .setAddress("127.0.0.1:9603"); // 设置注册中心地址
+
+        // 2. consumer 综合配置
+        ConsumerConfig<HelloService> consumerConfig = new ConsumerConfig<HelloService>()
+                .setInterfaceId(HelloService.class.getName()) // 指定接口
+                .setRegistry(registryConfig)
+                .setProtocol("bolt") // 指定协议
+                .setConnectTimeout(10 * 1000);
+
+        // 3. 构造服务引用
+        HelloService helloService = consumerConfig.refer();
+
+        // 4. 进行服务调用
+        System.out.println(helloService.sayHello("world"));
+    }
+}
---
+++ example/src/test/java/com/alipay/sofa/rpc/sofaregistry/SofaRegistryServer.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.rpc.sofaregistry;
+
+import com.alipay.sofa.rpc.config.ProviderConfig;
+import com.alipay.sofa.rpc.config.RegistryConfig;
+import com.alipay.sofa.rpc.config.ServerConfig;
+
+/**
+ * Quick Start Server
+ */
+public class SofaRegistryServer {
+
+    public static void main(String[] args) {
+        // 1. 注册中心配置
+        RegistryConfig registryConfig = new RegistryConfig()
+                .setProtocol("sofa") // 设置协议
+                .setAddress("127.0.0.1:9603"); // 设置注册中心地址
+
+        // 2. 通信服务配置
+        ServerConfig serverConfig = new ServerConfig()
+            .setProtocol("bolt") // 设置一个协议，默认bolt
+            .setPort(12200) // 设置一个端口，默认12200
+            .setDaemon(false); // 非守护线程
+
+        // 3. provider 综合配置
+        ProviderConfig<HelloService> providerConfig = new ProviderConfig<HelloService>()
+                .setRegistry(registryConfig)
+                .setInterfaceId(HelloService.class.getName()) // 指定接口
+                .setRef(new HelloServiceImpl()) // 指定实现
+                .setServer(serverConfig); // 指定服务端
+
+        providerConfig.export(); // 发布服务
+    }
+}
