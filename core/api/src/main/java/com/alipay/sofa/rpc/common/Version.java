f0e9ea5c1828b07e91931c392e28853c3613d7d1
leizhiyuan <leizhiyuan@gmail.com>
2019-06-06 17:18:39 +0800

    update version to 5.3.4

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.3.4_20180613175525";
+    public static final String BUILD_VERSION = "5.3.4_20190606171809";
 }
