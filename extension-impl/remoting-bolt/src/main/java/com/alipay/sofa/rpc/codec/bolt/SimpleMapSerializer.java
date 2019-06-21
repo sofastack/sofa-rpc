51ac1d2a4ab29de7e77cb2b5f064167a8218aa8f
yì jí <yiji@apache.org>
2019-06-21 15:56:10 +0800

    fix unit test failed. (#681)
    * fix unit test failed.
    
    * Avoid null key transfer to headers

--- extension-impl/remoting-bolt/src/main/java/com/alipay/sofa/rpc/codec/bolt/SimpleMapSerializer.java
+++ extension-impl/remoting-bolt/src/main/java/com/alipay/sofa/rpc/codec/bolt/SimpleMapSerializer.java
@@ -55,8 +55,13 @@ public class SimpleMapSerializer {
             for (Map.Entry<String, String> entry : map.entrySet()) {
                 String key = entry.getKey();
                 String value = entry.getValue();
-                writeSupportEmpty(key, out);
-                writeSupportEmpty(value, out);
+                /**
+                 * 排除不写null作为key
+                 */
+                if(key != null){
+                    writeSupportEmpty(key, out);
+                    writeSupportEmpty(value, out);
+                }
             }
             return out.toByteArray();
         } catch (IOException ex) {
