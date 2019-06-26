82fb49b085d54df4da185e3f54cc37d9e24ea116
zonghaishang <yiji@apache.org>
2019-06-26 16:59:04 +0800

    prepare for release 5.5.7

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.5.7_20190625203549";
+    public static final String BUILD_VERSION = "5.5.7_20190626165820";
 }
