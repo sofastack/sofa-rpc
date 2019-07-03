15b43e012981e190f9e37859afd711447a61fe47
QilongZhang <qilong.zql@alibaba-inc.com>
2019-07-03 14:36:00 +0800

    upgrade version to 5.5.7

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.5.6";
+    public static final String VERSION       = "5.5.7";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50506;
+    public static final int    RPC_VERSION   = 50507;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.5.6_20190613120839";
+    public static final String BUILD_VERSION = "5.5.7_20190703143354";
 }
