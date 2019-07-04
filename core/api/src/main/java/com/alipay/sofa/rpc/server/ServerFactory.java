29caf6cad2d33fdc2200e705b109385a92dba6fb
QilongZhang <qilong.zql@antfin.com>
2019-07-04 11:58:57 +0800

    Destroy server config. (#709)
    destroy server completely

--- core/api/src/main/java/com/alipay/sofa/rpc/server/ServerFactory.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/server/ServerFactory.java
@@ -149,4 +149,16 @@ public final class ServerFactory {
         }
         SERVER_MAP.clear();
     }
+
+    public static void destroyServer(ServerConfig serverConfig) {
+        try {
+            Server server = serverConfig.getServer();
+            if (server != null) {
+                SERVER_MAP.remove(Integer.toString(serverConfig.getPort()));
+                server.destroy();
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error when destroy server with key:" + serverConfig.getPort(), e);
+        }
+    }
 }
