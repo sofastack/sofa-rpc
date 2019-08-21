dca5eff72acc0a2ad3e0e22ed228defe785c1cb5
Lei Zhiyuan <leizhiyuan@gmail.com>
2019-08-21 16:39:29 +0800

    Prepare release 557. (#741)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.5.7_20190703143354";
+    public static final String BUILD_VERSION = "5.5.7_20190821142349";
 }
