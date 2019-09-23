1788857cef3ba1774db7f668d0c5a8282b09fbfa
Lei Zhiyuan <leizhiyuan@gmail.com>
2019-09-23 14:56:43 +0800

    Change rpc 5.6.2. (#760)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.6.2_20190907161033";
+    public static final String BUILD_VERSION = "5.6.2_20190916202911";
 }
