6837402e747cdbcc30a0cd867998bb862c019c90
Lei Zhiyuan <leizhiyuan@gmail.com>
2019-10-11 10:45:47 +0800

    Change version 559 snapshot. (#784)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.5.8";
+    public static final String VERSION       = "5.5.9";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50508;
+    public static final int    RPC_VERSION   = 50509;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.5.8_20190925155840";
+    public static final String BUILD_VERSION = "5.5.9_20191009073008";
 }
