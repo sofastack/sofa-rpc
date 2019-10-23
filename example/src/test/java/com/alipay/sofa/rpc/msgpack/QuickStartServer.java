a018a86c807a2d1cf746fa903a08b4e120801991
wengdezhi <wengdezhi@50yc.com>
2019-10-23 10:21:41 +0800

    foment the code

--- example/src/test/java/com/alipay/sofa/rpc/msgpack/QuickStartServer.java
+++ example/src/test/java/com/alipay/sofa/rpc/msgpack/QuickStartServer.java
@@ -27,15 +27,15 @@ public class QuickStartServer {
 
     public static void main(String[] args) {
         ServerConfig serverConfig = new ServerConfig()
-                .setProtocol(RpcConstants.PROTOCOL_TYPE_BOLT) // 设置一个协议，默认bolt
-                .setPort(12200) // 设置一个端口，默认12200
-                .setSerialization("msgpack")
-                .setDaemon(false); // 非守护线程
+            .setProtocol(RpcConstants.PROTOCOL_TYPE_BOLT) // 设置一个协议，默认bolt
+            .setPort(12200) // 设置一个端口，默认12200
+            .setSerialization("msgpack")
+            .setDaemon(false); // 非守护线程
 
         ProviderConfig<HelloService> providerConfig = new ProviderConfig<HelloService>()
-                .setInterfaceId(HelloService.class.getName()) // 指定接口
-                .setRef(new HelloServiceImpl()) // 指定实现
-                .setServer(serverConfig); // 指定服务端
+            .setInterfaceId(HelloService.class.getName()) // 指定接口
+            .setRef(new HelloServiceImpl()) // 指定实现
+            .setServer(serverConfig); // 指定服务端
 
         providerConfig.export(); // 发布服务
     }
