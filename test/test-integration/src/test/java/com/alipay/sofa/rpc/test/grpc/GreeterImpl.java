dcd34599fac6752d024f244e6e212bdd95d4e0e0
leizhiyuan <leizhiyuan@gmail.com>
2019-12-09 10:32:52 +0800

    test:fix test case

--- test/test-integration/src/test/java/com/alipay/sofa/rpc/test/grpc/GreeterImpl.java
+++ test/test-integration/src/test/java/com/alipay/sofa/rpc/test/grpc/GreeterImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alipay.sofa.rpc.bootstrap.grpc.demo;
+package com.alipay.sofa.rpc.test.grpc;
 
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
