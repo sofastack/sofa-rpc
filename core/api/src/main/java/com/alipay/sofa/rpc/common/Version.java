5d961916aae1f54a1583a10ac943f65309ddb826
leizhiyuan <leizhiyuan@gmail.com>
2020-02-18 10:58:59 +0800

    chore:update rpc version 5.6.4

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.6.4_20191217120018";
+    public static final String BUILD_VERSION = "5.6.4_20200218105828";
 }
