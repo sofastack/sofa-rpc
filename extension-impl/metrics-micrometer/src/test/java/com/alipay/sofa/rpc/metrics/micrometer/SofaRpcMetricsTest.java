8627a667ab4f1cf9932c9d2cfdd051db6a48eb4c
hujia <hujia@topjoy.com>
2020-03-04 18:52:25 +0800

    after run mvn complie

--- extension-impl/metrics-micrometer/src/test/java/com/alipay/sofa/rpc/metrics/micrometer/SofaRpcMetricsTest.java
+++ extension-impl/metrics-micrometer/src/test/java/com/alipay/sofa/rpc/metrics/micrometer/SofaRpcMetricsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.rpc.metrics.micrometer;
 
 import com.alipay.sofa.rpc.common.RemotingConstants;
@@ -68,8 +84,8 @@ public class SofaRpcMetricsTest {
         request.setInterfaceName(TestService.class.getName());
         request.setMethodName("echoStr");
         request.setMethod(TestService.class.getMethod("func"));
-        request.setMethodArgs(new Object[] { });
-        request.setMethodArgSigs(new String[] { });
+        request.setMethodArgs(new Object[] {});
+        request.setMethodArgSigs(new String[] {});
         request.setTargetServiceUniqueName(TestService.class.getName() + ":1.0");
         request.setTargetAppName("targetApp");
         request.setSerializeType((byte) 11);
