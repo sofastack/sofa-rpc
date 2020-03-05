aa26f0ca4391402dffdaa8f1e9db2e40db5927bc
Lei Zhiyuan <leizhiyuan@gmail.com>
2020-03-05 11:01:40 +0800

    chore: add code for sofaboot (#867)

--- core/api/src/main/java/com/alipay/sofa/rpc/log/LogCodes.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/log/LogCodes.java
@@ -168,6 +168,7 @@ public class LogCodes {
     public static final String                 ERROR_WATCH_HEALTH                         = "010060025";
     public static final String                 ERROR_CLOSE_PATH_CACHE                     = "010060026";
     public static final String                 ERROR_INVALID_ATTRIBUTE                    = "010060027";
+    public static final String                 ERROR_REGISTRY_NOT_SUPPORT                 = "010060028";
 
     //01007 log
     //01008 proxy generate
@@ -213,6 +214,7 @@ public class LogCodes {
     public static final String                 ERROR_HOST_NOT_FOUND                       = "019990024";
     public static final String                 ERROR_QUERY_ATTRIBUTE                      = "019990025";
     public static final String                 ERROR_UPDATE_ATTRIBUTE                     = "019990026";
+    public static final String                 ERROR_SERVER_PROTOCOL_NOT_SUPPORT          = "019990027";
 
     //02 运行
     // 02000 泛化
