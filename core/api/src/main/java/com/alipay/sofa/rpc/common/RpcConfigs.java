b71b87c4f685db28ce06f98d8875e9de8db1e522
zhangchengxi <orezsilence@163.com>
2020-04-07 13:21:29 +0800

    fix: happen to delete one line

--- core/api/src/main/java/com/alipay/sofa/rpc/common/RpcConfigs.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/RpcConfigs.java
@@ -73,6 +73,7 @@ public class RpcConfigs {
             loadCustom("META-INF/sofa-rpc/rpc-config.json");
 
             // load system properties
+            CFG.putAll(new HashMap(System.getProperties())); // 注意部分属性可能被覆盖为字符串
         } catch (Exception e) {
             throw new SofaRpcRuntimeException(LogCodes.getLog(LogCodes.ERROR_LOAD_RPC_CONFIGS), e);
         }
