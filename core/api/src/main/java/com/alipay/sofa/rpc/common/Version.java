b2aa1862424341aa6d44656b154bf530d916c02a
张呈熹 <orezsilence@163.com>
2020-04-14 20:05:23 +0800

    chore:upgrade version to 5.6.6-SNAPSHOT (#887)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.6.5";
+    public static final String VERSION       = "5.6.6";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50605;
+    public static final int    RPC_VERSION   = 50606;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.6.5_20200313112735";
+    public static final String BUILD_VERSION = "5.6.6_20200317143947";
 }
