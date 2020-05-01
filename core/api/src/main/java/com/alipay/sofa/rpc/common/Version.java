8e566b540816bc946fd4fdd14e2842b0d0c32979
Lei Zhiyuan <leizhiyuan@gmail.com>
2020-05-01 14:50:03 +0800

    Chore/release570 (#908)
    * chore:rpc 570
    
    * chore:compiler 0.0.1

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.0_20200310125011";
+    public static final String BUILD_VERSION = "5.7.0_20200429175905";
 }
