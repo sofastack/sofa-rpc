4256702ba6bc656c2c53160efbef50cb6d416044
Lei Zhiyuan <leizhiyuan@gmail.com>
2020-05-16 14:23:30 +0800

    chore:change rpc version 5.7.1 (#920)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,5 +37,5 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.1_20200508140739";
+    public static final String BUILD_VERSION = "5.7.1_20200516120545";
 }
