f8aae68d2a387a4ed5c12e116281c0819ebb3f7e
Lei Zhiyuan <leizhiyuan@gmail.com>
2020-05-19 11:10:07 +0800

    chore:5.7.2 snapshot (#922)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.7.1";
+    public static final String VERSION       = "5.7.2";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50701;
+    public static final int    RPC_VERSION   = 50702;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.1_20200516120545";
+    public static final String BUILD_VERSION = "5.7.2_20200519110714";
 }
