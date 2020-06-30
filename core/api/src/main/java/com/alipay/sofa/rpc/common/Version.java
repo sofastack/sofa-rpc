fd1377efb51df3f3ed91927d9386ff8be1ea5458
张呈熹 <orezsilence@163.com>
2020-06-30 11:19:58 +0800

    chore:upgrade version to 5.7.4-SNAPHOST (#959)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.7.3";
+    public static final String VERSION       = "5.7.4";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50703;
+    public static final int    RPC_VERSION   = 50704;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.3_20200621151545";
+    public static final String BUILD_VERSION = "5.7.4_20200630110321";
 }
