9bd0e077bbdab8e2296767dbdd718f4f41b5b1c5
张呈熹 <orezsilence@163.com>
2020-09-24 19:37:38 +0800

    upgrade version to 5.7.6 (#975)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,15 +27,15 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.7.5";
+    public static final String VERSION       = "5.7.6";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50705;
+    public static final int    RPC_VERSION   = 50706;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.5_20200731150727";
+    public static final String BUILD_VERSION = "5.7.6_20200908155956";
 }
