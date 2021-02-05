804fa788fb304530293c6f607c2e08fb7aa89c5c
张呈熹 <orezsilence@163.com>
2021-02-05 13:36:34 +0800

    upgrade versiont to 5.7.7 (#1018)

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.7_20201225155429";
+    public static final String BUILD_VERSION = "5.7.7_20210129173053";
 
 }
