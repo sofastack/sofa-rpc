16f9ca782f7f2e9ce0b4705def72a2aba0179f6d
卫恒 <guolei.sgl@antfin.com>
2021-03-25 12:01:09 +0800

    upgrade-5.7.8-SNAPSHOT

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.7.7";
+    public static final String VERSION       = "5.7.8";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50707;
+    public static final int    RPC_VERSION   = 50708;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.7_20210129173053";
+    public static final String BUILD_VERSION = "5.7.8_20210325115835";
 
 }
