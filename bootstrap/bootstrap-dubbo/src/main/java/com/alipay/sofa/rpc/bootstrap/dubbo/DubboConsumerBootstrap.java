826c1aabf1238b0f4c22a75cc71160aa14660435
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2021-05-08 17:45:40 +0800

    submit

--- bootstrap/bootstrap-dubbo/src/main/java/com/alipay/sofa/rpc/bootstrap/dubbo/DubboConsumerBootstrap.java
+++ bootstrap/bootstrap-dubbo/src/main/java/com/alipay/sofa/rpc/bootstrap/dubbo/DubboConsumerBootstrap.java
@@ -53,6 +53,11 @@ public class DubboConsumerBootstrap<T> extends ConsumerBootstrap<T> {
     protected transient volatile T proxyIns;
 
     /**
+     * dubbo service version
+     */
+    private static final String    VERSION = "version";
+
+    /**
      * 构造函数
      *
      * @param consumerConfig 服务消费者配置
