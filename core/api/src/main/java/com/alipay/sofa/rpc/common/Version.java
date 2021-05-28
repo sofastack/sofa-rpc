57acbf8538087ffc94c9d950a8b6e625b6851158
evenliu <evenljj@163.com>
2021-05-27 22:23:14 -0500

    update version to 5.7.8 (#1045)
    Co-authored-by: liujianjun.ljj <liujianjun.ljj@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.8_20210325115835";
+    public static final String BUILD_VERSION = "5.7.8_20210528105236";
 
 }
