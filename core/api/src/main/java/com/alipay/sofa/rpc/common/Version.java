8425ff4d83adc292569ec5685fdae9829f326234
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2021-06-03 19:54:49 +0800

    update version to 5.7.9-SNAPSHOT

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.7.8";
+    public static final String VERSION       = "5.7.9";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50708;
+    public static final int    RPC_VERSION   = 50709;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.8_20210528105236";
+    public static final String BUILD_VERSION = "5.7.9_20210603195343";
 
 }
