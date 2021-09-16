2d9c93019b348ef123099ceeeb5d1164aed89853
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2021-09-16 22:59:46 +0800

    upgrade rpv version to 5.7.10

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.7.10_20210721164918";
+    public static final String BUILD_VERSION = "5.7.10_20210916225802";
 
 }
