94c3dc36b971f8fd50594c42cef635ea98050df5
evenliu <evenljj@163.com>
2021-11-26 03:33:35 -0600

    Upgrade rpc 581 (#1135)
    * Upgrade rpc version to 5.8.1
    
    Co-authored-by: liujianjun.ljj <liujianjun.ljj@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.8.1_20211103203858";
+    public static final String BUILD_VERSION = "5.8.1_20211126120535";
 
 }
