f9e6558075e99a355d7609a98f2b124d26bdcc7b
三樵 <liwu.clw@antgroup.com>
2022-02-09 11:34:59 +0800

    ut

--- core/api/src/test/java/com/alipay/sofa/rpc/config/UserThreadPoolManagerTest.java
+++ core/api/src/test/java/com/alipay/sofa/rpc/config/UserThreadPoolManagerTest.java
@@ -33,20 +33,21 @@ public class UserThreadPoolManagerTest {
 
         UserThreadPoolManager.registerUserThread("service1", pool1);
         userThreadPoolSet = UserThreadPoolManager.getUserThreadPoolSet();
-        Assert.assertTrue(userThreadPoolSet.size() == 1);
+        Assert.assertEquals(1, userThreadPoolSet.size());
 
         UserThreadPool pool2 = new UserThreadPool();
         Assert.assertTrue(pool2.getThreadPoolName().startsWith(UserThreadPool.DEFAUT_POOL_NAME + "-"));
+        Assert.assertNotEquals(pool2.getThreadPoolName(), pool1.getThreadPoolName());
 
         UserThreadPoolManager.registerUserThread("service2", pool2);
         userThreadPoolSet = UserThreadPoolManager.getUserThreadPoolSet();
-        Assert.assertTrue(userThreadPoolSet.size() == 2);
+        Assert.assertEquals(2, userThreadPoolSet.size());
 
         UserThreadPool pool3 = new UserThreadPool("samePoolName");
         UserThreadPoolManager.registerUserThread("service3", pool3);
         UserThreadPool pool4 = new UserThreadPool("samePoolName");
         UserThreadPoolManager.registerUserThread("service4", pool4);
         userThreadPoolSet = UserThreadPoolManager.getUserThreadPoolSet();
-        Assert.assertTrue(userThreadPoolSet.size() == 4);
+        Assert.assertEquals(4, userThreadPoolSet.size());
     }
 }
