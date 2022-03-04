c6df49d1352cfc8449d21967f1f8cdad8f566de8
evenliu <evenljj@163.com>
2022-03-04 14:42:09 +0800

    upgrade rpc version to 5.8.3 (#1177)
    Co-authored-by: liujianjun.ljj <liujianjun.ljj@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.8.3_20220104204953";
+    public static final String BUILD_VERSION = "5.8.3_20220304140022";
 
 }
