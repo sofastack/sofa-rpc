e21cbd43ed4c06d2ad58b82759b12a7cea71dd19
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2022-05-08 18:49:10 +0800

    avoid concurrency problem in DEQUE_LOCAL

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.8.3";
+    public static final String VERSION       = "5.8.3.1";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50803;
+    public static final int    RPC_VERSION   = 508301;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.8.3_20220304140022";
+    public static final String BUILD_VERSION = "5.8.3.1_20220508180257";
 
 }
