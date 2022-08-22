d190a84f76e9af3828aa60177a10ca055f1d3448
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2022-08-22 20:32:47 +0800

    remove appendFilePath, can use Paths.get().toString() instead

--- core/common/src/main/java/com/alipay/sofa/rpc/common/utils/FileUtils.java
+++ core/common/src/main/java/com/alipay/sofa/rpc/common/utils/FileUtils.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
