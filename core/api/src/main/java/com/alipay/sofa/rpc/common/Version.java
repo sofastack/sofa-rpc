ba6bf69790448770d34ede2831b3174a8cce70c4
evenliu <evenljj@163.com>
2022-10-26 17:53:58 +0800

    Revert "upgrade rpc version to 5.8.8 (#1267)"
    This reverts commit a346a7ed7255f866714c86c1661baed1a6575781.

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.8.8";
+    public static final String VERSION       = "5.8.7";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50808;
+    public static final int    RPC_VERSION   = 50807;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.8.8_20221026173922";
+    public static final String BUILD_VERSION = "5.8.7_20220822135526";
 
 }
