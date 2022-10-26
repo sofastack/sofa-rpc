354332b3bf958cd6149805fbc73b83d4f72e6e05
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2022-10-26 12:18:35 +0800

    remove rpc version modify

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.8.7_20220920203452";
+    public static final String BUILD_VERSION = "5.8.7_20220822135526";
 
 }
