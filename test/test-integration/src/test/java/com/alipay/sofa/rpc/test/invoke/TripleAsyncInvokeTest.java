d8849e91d2d5c2b0c072c2afd037d700a7054161
Marco <lingfenglangshao@gmail.com>
2022-11-01 15:25:36 +0800

    format code

--- test/test-integration/src/test/java/com/alipay/sofa/rpc/test/invoke/TripleAsyncInvokeTest.java
+++ test/test-integration/src/test/java/com/alipay/sofa/rpc/test/invoke/TripleAsyncInvokeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.rpc.test.invoke;
 
 import com.alipay.sofa.rpc.api.future.SofaResponseFuture;
@@ -35,32 +51,32 @@ public class TripleAsyncInvokeTest {
     @BeforeClass
     public static void start() {
         ServerConfig serverConfig2 = new ServerConfig()
-                .setPort(22223)
-                .setProtocol(RpcConstants.PROTOCOL_TYPE_TRIPLE)
-                .setDaemon(false);
+            .setPort(22223)
+            .setProtocol(RpcConstants.PROTOCOL_TYPE_TRIPLE)
+            .setDaemon(false);
 
         // 服务端
         ProviderConfig<HelloService> CProvider = new ProviderConfig<HelloService>()
-                .setInterfaceId(HelloService.class.getName())
-                .setRef(new HelloServiceImpl(1000))
-                .setServer(serverConfig2);
+            .setInterfaceId(HelloService.class.getName())
+            .setRef(new HelloServiceImpl(1000))
+            .setServer(serverConfig2);
         CProvider.export();
 
         // 客户端
         ConsumerConfig<HelloService> BConsumer = new ConsumerConfig<HelloService>()
-                .setInterfaceId(HelloService.class.getName())
-                .setInvokeType(RpcConstants.INVOKER_TYPE_FUTURE)
-                .setTimeout(5000)
-                .setProtocol(RpcConstants.PROTOCOL_TYPE_TRIPLE)
-                .setDirectUrl("127.0.0.1:22223");
+            .setInterfaceId(HelloService.class.getName())
+            .setInvokeType(RpcConstants.INVOKER_TYPE_FUTURE)
+            .setTimeout(5000)
+            .setProtocol(RpcConstants.PROTOCOL_TYPE_TRIPLE)
+            .setDirectUrl("127.0.0.1:22223");
         future = BConsumer.refer();
 
         ConsumerConfig<HelloService> BBConsumer = new ConsumerConfig<HelloService>()
-                .setInterfaceId(HelloService.class.getName())
-                .setInvokeType(RpcConstants.INVOKER_TYPE_CALLBACK)
-                .setTimeout(5000)
-                .setProtocol(RpcConstants.PROTOCOL_TYPE_TRIPLE)
-                .setDirectUrl("127.0.0.1:22223");
+            .setInterfaceId(HelloService.class.getName())
+            .setInvokeType(RpcConstants.INVOKER_TYPE_CALLBACK)
+            .setTimeout(5000)
+            .setProtocol(RpcConstants.PROTOCOL_TYPE_TRIPLE)
+            .setDirectUrl("127.0.0.1:22223");
         callback = BBConsumer.refer();
     }
 
