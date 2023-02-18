330974c9f7b826aaaf3302f87d174a9454cf7e2e
Khotyn Huang <hting1@gmail.com>
2023-02-18 10:47:18 +0800

    Fix compatible problem with mac chips.
    Fix #1302

--- remoting/remoting-triple/build/generated/source/proto/main/java/triple/RequestOrBuilder.java
+++ remoting/remoting-triple/build/generated/source/proto/main/java/triple/RequestOrBuilder.java
@@ -9,42 +9,54 @@ public interface RequestOrBuilder extends
 
   /**
    * <code>string serializeType = 1;</code>
+   * @return The serializeType.
    */
   java.lang.String getSerializeType();
   /**
    * <code>string serializeType = 1;</code>
+   * @return The bytes for serializeType.
    */
   com.google.protobuf.ByteString
       getSerializeTypeBytes();
 
   /**
    * <code>repeated bytes args = 2;</code>
+   * @return A list containing the args.
    */
   java.util.List<com.google.protobuf.ByteString> getArgsList();
   /**
    * <code>repeated bytes args = 2;</code>
+   * @return The count of args.
    */
   int getArgsCount();
   /**
    * <code>repeated bytes args = 2;</code>
+   * @param index The index of the element to return.
+   * @return The args at the given index.
    */
   com.google.protobuf.ByteString getArgs(int index);
 
   /**
    * <code>repeated string argTypes = 3;</code>
+   * @return A list containing the argTypes.
    */
   java.util.List<java.lang.String>
       getArgTypesList();
   /**
    * <code>repeated string argTypes = 3;</code>
+   * @return The count of argTypes.
    */
   int getArgTypesCount();
   /**
    * <code>repeated string argTypes = 3;</code>
+   * @param index The index of the element to return.
+   * @return The argTypes at the given index.
    */
   java.lang.String getArgTypes(int index);
   /**
    * <code>repeated string argTypes = 3;</code>
+   * @param index The index of the value to return.
+   * @return The bytes of the argTypes at the given index.
    */
   com.google.protobuf.ByteString
       getArgTypesBytes(int index);
