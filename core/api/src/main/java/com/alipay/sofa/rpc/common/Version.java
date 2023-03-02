fc9500125cadb6055c8769c1ed164c39534dfc80
junyuan <zhangminglun.zml@ant-group.com>
2023-03-02 14:54:18 +0800

    feat: 5.9.2 release

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.9.2_20230220172555";
+    public static final String BUILD_VERSION = "5.9.2_20230302145156";
 
 }
