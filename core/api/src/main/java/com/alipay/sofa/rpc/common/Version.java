7fe66dbb42b0f425426533437e8c3f130c4e88d2
junyuan <zhangminglun.zml@ant-group.com>
2023-03-29 15:06:46 +0800

    version

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.9.2";
+    public static final String VERSION       = "5.10.017";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 50902;
+    public static final int    RPC_VERSION   = 510017;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.9.2_20230302150218";
+    public static final String BUILD_VERSION = "5.10.017_20230329134616";
 
 }
