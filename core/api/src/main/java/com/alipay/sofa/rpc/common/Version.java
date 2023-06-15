a4d551cbc2dd9b1d81fbf276f10d9a6871925c21
Lo1nt <zhangminglun.zml@antgroup.com>
2023-06-15 17:19:17 +0800

    updapte 5.10.1 version to release

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.10.1_20230607195037";
+    public static final String BUILD_VERSION = "5.10.1_20230615171852";
 
 }
