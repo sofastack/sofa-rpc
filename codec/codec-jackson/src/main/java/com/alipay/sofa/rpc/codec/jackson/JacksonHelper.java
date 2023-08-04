1778b126da845d679ccaa4aae22d6e0d65b6c724
淼淼 <lmm01746983@antgroup.com>
2023-08-04 16:02:18 +0800

    1.Modified version, this extends father object non-ParameterizedType, father object/super object definition apply method, and definition requestType and responseType, type change fixed success 2. The TestRequest2 method is provided to test the problem of parent class non-ParameterizedType conversion

--- codec/codec-jackson/src/main/java/com/alipay/sofa/rpc/codec/jackson/JacksonHelper.java
+++ codec/codec-jackson/src/main/java/com/alipay/sofa/rpc/codec/jackson/JacksonHelper.java
@@ -122,13 +122,13 @@ public class JacksonHelper {
         if (jsonMethod.getDeclaringClass() != clazz && !clazz.isInterface()) {
             parameterTypes = new Type[1];
             Type type = clazz.getGenericSuperclass();
-            try{
+            try {
                 ParameterizedType parameterizedType = (ParameterizedType) type;
                 // parse request types
                 parameterTypes[0] = parameterizedType.getActualTypeArguments()[0];
                 // parse response types
                 resType = parameterizedType.getActualTypeArguments()[1];
-            }catch (ClassCastException e){
+            } catch (ClassCastException e) {
                 // parse request types
                 parameterTypes = jsonMethod.getGenericParameterTypes();
                 // parse response types
