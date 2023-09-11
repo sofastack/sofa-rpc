e3b64e607dafaa25a7574a2649b63ec5110defc6
Jason Song <songshun.ss@antfin.com>
2023-09-11 10:16:34 +0800

    upgrade rpc version to 5.11.1-SNAPSHOT (#1366)
    Co-authored-by: Jason Song <songshun.ss@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.11.0";
+    public static final String VERSION       = "5.11.1";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 51100;
+    public static final int    RPC_VERSION   = 51101;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.11.0_20230828164355";
+    public static final String BUILD_VERSION = "5.11.1_20230911090800";
 
 }
