f37465bf30bb89322e4911dd827495b88b4bdcac
lo1nt <minglunz@student.unimelb.edu.au>
2024-01-22 15:04:25 +0800

    release 5.12.0 (#1394)
    Co-authored-by: Lo1nt <zhangminglun.zml@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -27,16 +27,16 @@ public final class Version {
     /**
      * 当前RPC版本，例如：5.6.7
      */
-    public static final String VERSION       = "5.11.1";
+    public static final String VERSION       = "5.12.0";
 
     /**
      * 当前RPC版本，例如： 5.6.7 对应 50607
      */
-    public static final int    RPC_VERSION   = 51101;
+    public static final int    RPC_VERSION   = 51200;
 
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.11.1_20230911155412";
+    public static final String BUILD_VERSION = "5.12.0_20240122111527";
 
 }
