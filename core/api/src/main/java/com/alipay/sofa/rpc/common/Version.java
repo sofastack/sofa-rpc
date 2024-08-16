cdbd574857c722309f31e4b65f3931479304ca86
evenliu <evenljj@163.com>
2024-08-16 17:14:27 +0800

    enhance log format (#1437)
    Co-authored-by: liujianjun.ljj <liujianjun.ljj@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.13.1_20240607152350";
+    public static final String BUILD_VERSION = "5.13.1_20240815203044";
 
 }
