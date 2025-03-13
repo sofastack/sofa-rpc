eee71f3634256426f1784e2438c2df1b90a247d3
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2025-03-13 19:50:17 +0800

    5.13.3 release

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.13.3_20250117153647";
+    public static final String BUILD_VERSION = "5.13.3_20250313194940";
 
 }
