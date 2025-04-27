af39a9f56e1d55b709e5c699b9696eea96483eb5
liujianjun.ljj <liujianjun.ljj@antgroup.com>
2025-04-27 14:10:52 +0800

    update rpc version release to 5.13.4

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.13.4_20250410204629";
+    public static final String BUILD_VERSION = "5.13.4_20250427140941";
 
 }
