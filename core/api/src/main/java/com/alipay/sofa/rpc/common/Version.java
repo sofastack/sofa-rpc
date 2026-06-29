2e0909dab280a5e10f9161fa9ec520f0855792da
evenliu <evenljj@163.com>
2026-06-29 07:25:30 -0500

    update rpc version to 5.14.3 (#1568)
    Co-authored-by: liujianjun.ljj <liujianjun.ljj@antgroup.com>

--- core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
+++ core/api/src/main/java/com/alipay/sofa/rpc/common/Version.java
@@ -37,6 +37,6 @@ public final class Version {
     /**
      * 当前Build版本，每次发布修改
      */
-    public static final String BUILD_VERSION = "5.14.3_20260316115815";
+    public static final String BUILD_VERSION = "5.14.3_20260629200518";
 
 }
